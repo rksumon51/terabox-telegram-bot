0a449455e913da1661f4b09a6ede678217d8e484
rksumon51 <rk.sumon51@gmail.com>
2026-03-13 15:54:45 +0600

    Refactor get_download_link to use regex for link

--- terabox.py
+++ terabox.py
@@ -1,10 +1,20 @@
 import requests
+import re
 
 def get_download_link(url):
-    api = f"https://teraboxdownloader.nepcoderdevs.workers.dev/?url={url}"
-    data = requests.get(api).json()
 
-    if "download" in data:
-        return data["download"]
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    r = requests.get(url, headers=headers)
+
+    html = r.text
+
+    match = re.search(r'"downloadUrl":"(.*?)"', html)
+
+    if match:
+        link = match.group(1).replace("\\/", "/")
+        return link
     else:
         raise Exception("Download link not found")
